642c07f44c8b6490e02ad4dcc91674b8f529b71f
Sergen ORIN <serg_or5@hotmail.com>
2022-07-20 12:32:58 +0300

    changed svgs with more optimised versions

--- components/VerticalNavbar.vue
+++ components/VerticalNavbar.vue
@@ -90,43 +90,30 @@
       </nav>
     </div>
     <div class="w-full">
-      <div class="w-full h-16 grid place-items-center">
+      <div class="w-full h-16 py-2 grid place-items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
           width="27.628"
           height="27.579"
           viewBox="0 0 27.628 27.579"
         >
-          <defs>
-            <clipPath id="clip-path">
-              <rect
-                id="Rectangle_41"
-                data-name="Rectangle 41"
-                width="27.628"
-                height="27.579"
-                fill="#ea2044"
-              />
-            </clipPath>
-          </defs>
-          <g id="Group_25" data-name="Group 25" clip-path="url(#clip-path)">
-            <path
-              id="Path_10"
-              data-name="Path 10"
-              d="M.454,9.157H6.181V27.579H.454ZM3.318,0A3.32,3.32,0,1,1,0,3.322,3.322,3.322,0,0,1,3.318,0"
-              fill="#ea2044"
-            />
-            <path
-              id="Path_11"
-              data-name="Path 11"
-              d="M157.107,140.371H162.6v2.52h.079a6.012,6.012,0,0,1,5.417-2.975c5.8,0,6.869,3.814,6.869,8.773v10.1H169.24v-8.958c0-2.136-.037-4.886-2.976-4.886-2.979,0-3.434,2.33-3.434,4.732v9.112h-5.723Z"
-              transform="translate(-147.336 -131.214)"
-              fill="#ea2044"
-            />
-          </g>
+          <path
+            id="Path_10"
+            data-name="Path 10"
+            d="M.454,9.157H6.181V27.579H.454ZM3.318,0A3.32,3.32,0,1,1,0,3.322,3.322,3.322,0,0,1,3.318,0"
+            transform="translate(0)"
+            fill="#ea2044"
+          />
+          <path
+            id="Path_11"
+            data-name="Path 11"
+            d="M157.107,140.371H162.6v2.52h.079a6.012,6.012,0,0,1,5.417-2.975c5.8,0,6.869,3.814,6.869,8.773v10.1H169.24v-8.958c0-2.136-.037-4.886-2.976-4.886-2.979,0-3.434,2.33-3.434,4.732v9.112h-5.723Z"
+            transform="translate(-147.336 -131.214)"
+            fill="#ea2044"
+          />
         </svg>
       </div>
-      <div class="w-full h-16 grid place-items-center">
+      <div class="w-full h-16 py-2 grid place-items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="33.355"
@@ -137,7 +124,7 @@
             id="Path_22"
             data-name="Path 22"
             d="M26.939,6.318a1.955,1.955,0,1,0,1.955,1.955,1.956,1.956,0,0,0-1.955-1.955"
-            transform="translate(-1.431 -0.361)"
+            transform="translate(-1.431 -0.362)"
             fill="#fff"
           />
           <path
@@ -151,7 +138,6 @@
             id="Path_24"
             data-name="Path 24"
             d="M23.336,33.356H10.019A10.03,10.03,0,0,1,0,23.337V10.019A10.03,10.03,0,0,1,10.019,0H23.336A10.031,10.031,0,0,1,33.355,10.019V23.337A10.031,10.031,0,0,1,23.336,33.356M10.019,3.138a6.888,6.888,0,0,0-6.881,6.881V23.337a6.888,6.888,0,0,0,6.881,6.881H23.336a6.888,6.888,0,0,0,6.881-6.881V10.019a6.888,6.888,0,0,0-6.881-6.881Z"
-            transform="translate(0 0)"
             fill="#fff"
           />
         </svg>
