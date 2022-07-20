0873e5acd499ef85a1b2b488eccfb21179bccb2c
Sergen ORIN <serg_or5@hotmail.com>
2022-07-20 22:56:51 +0300

    backup

--- pages/index.vue
+++ pages/index.vue
@@ -1,167 +1,61 @@
 <template>
-  <div class="grid place-items-center h-screen bg-gray-700 pt-10">
+  <div class="h-[5000px] bg-gray-700">
     <div
       class="
-        flex flex-col
-        justify-between
-        divide-y divide-white divide-opacity-50
-        h-3/5
-        w-24
-        bg-primary bg-opacity-40
-        backdrop-blur-lg
-        border border-white border-opacity-50
+        main-section
+        relative
+        w-full
+        bg-cover bg-center bg-no-repeat
+        min-h-screen
       "
     >
-      <div>
-        <div class="w-full text-center text-white text-base">
-          <div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="57"
-              height="26"
-              viewBox="0 0 57 26"
-              class="mx-auto my-2"
+      <div class="flex justify-center w-full relative top-7">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="663"
+          height="430"
+          viewBox="0 0 663 430"
+          class=""
+        >
+          <defs>
+            <pattern
+              id="pattern"
+              preserveAspectRatio="none"
+              width="100%"
+              height="100%"
+              viewBox="0 0 588 381"
             >
-              <rect
-                id="Rectangle_11"
-                data-name="Rectangle 11"
-                width="31"
-                height="3"
-                transform="translate(23)"
-                fill="#fff"
-              />
-              <rect
-                id="Rectangle_26"
-                data-name="Rectangle 26"
-                width="31"
-                height="3"
-                transform="translate(26 23)"
-                fill="#fff"
-              />
-              <rect
-                id="Rectangle_23"
-                data-name="Rectangle 23"
-                width="10"
-                height="3"
-                transform="translate(10)"
-                fill="#fff"
-              />
-              <rect
-                id="Rectangle_25"
-                data-name="Rectangle 25"
-                width="10"
-                height="3"
-                transform="translate(13 23)"
-                fill="#fff"
-              />
-              <rect
-                id="Rectangle_24"
-                data-name="Rectangle 24"
-                width="10"
-                height="3"
-                transform="translate(37 12)"
-                fill="#fff"
-              />
-              <rect
-                id="Rectangle_12"
-                data-name="Rectangle 12"
-                width="32"
-                height="3"
-                transform="translate(0 12)"
-                fill="#fff"
-              />
-            </svg>
-          </div>
-          MENU
-        </div>
-        <nav>
-          <ul class="text-right">
-            <li><a href="#about">HAKKINDA</a></li>
-            <li><a href="#program">PROGRAM</a></li>
-            <li><a href="#who-can-join">KİMLER KATILABİLİR?</a></li>
-            <li><a href="#what-will-i-win">NELER KAZANACAĞIM?</a></li>
-            <li><a href="#application-process">BAŞVURU SÜREÇLERİ</a></li>
-            <li><a href="#faq">SIKÇA SORULAN SORULAR</a></li>
-          </ul>
-        </nav>
+              <image width="588" height="381" href="~assets/images/badge.png" />
+            </pattern>
+          </defs>
+          <rect id="badge" width="663" height="430" fill="url(#pattern)" />
+        </svg>
       </div>
-      <div class="w-full">
-        <div class="w-full h-16 grid place-items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            width="27.628"
-            height="27.579"
-            viewBox="0 0 27.628 27.579"
-          >
-            <defs>
-              <clipPath id="clip-path">
-                <rect
-                  id="Rectangle_41"
-                  data-name="Rectangle 41"
-                  width="27.628"
-                  height="27.579"
-                  fill="#ea2044"
-                />
-              </clipPath>
-            </defs>
-            <g id="Group_25" data-name="Group 25" clip-path="url(#clip-path)">
-              <path
-                id="Path_10"
-                data-name="Path 10"
-                d="M.454,9.157H6.181V27.579H.454ZM3.318,0A3.32,3.32,0,1,1,0,3.322,3.322,3.322,0,0,1,3.318,0"
-                fill="#ea2044"
-              />
-              <path
-                id="Path_11"
-                data-name="Path 11"
-                d="M157.107,140.371H162.6v2.52h.079a6.012,6.012,0,0,1,5.417-2.975c5.8,0,6.869,3.814,6.869,8.773v10.1H169.24v-8.958c0-2.136-.037-4.886-2.976-4.886-2.979,0-3.434,2.33-3.434,4.732v9.112h-5.723Z"
-                transform="translate(-147.336 -131.214)"
-                fill="#ea2044"
-              />
-            </g>
-          </svg>
-        </div>
-        <div class="w-full h-16 grid place-items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="33.355"
-            height="33.356"
-            viewBox="0 0 33.355 33.356"
-          >
-            <path
-              id="Path_22"
-              data-name="Path 22"
-              d="M26.939,6.318a1.955,1.955,0,1,0,1.955,1.955,1.956,1.956,0,0,0-1.955-1.955"
-              transform="translate(-1.431 -0.361)"
-              fill="#fff"
-            />
-            <path
-              id="Path_23"
-              data-name="Path 23"
-              d="M17.337,8.977a8.215,8.215,0,1,0,8.214,8.215,8.224,8.224,0,0,0-8.214-8.215m0,13.476A5.262,5.262,0,1,1,22.6,17.192a5.268,5.268,0,0,1-5.261,5.261"
-              transform="translate(-0.522 -0.514)"
-              fill="#fff"
-            />
-            <path
-              id="Path_24"
-              data-name="Path 24"
-              d="M23.336,33.356H10.019A10.03,10.03,0,0,1,0,23.337V10.019A10.03,10.03,0,0,1,10.019,0H23.336A10.031,10.031,0,0,1,33.355,10.019V23.337A10.031,10.031,0,0,1,23.336,33.356M10.019,3.138a6.888,6.888,0,0,0-6.881,6.881V23.337a6.888,6.888,0,0,0,6.881,6.881H23.336a6.888,6.888,0,0,0,6.881-6.881V10.019a6.888,6.888,0,0,0-6.881-6.881Z"
-              transform="translate(0 0)"
-              fill="#fff"
-            />
-          </svg>
-        </div>
+      <div class="absolute w-full flex justify-around bottom-20">
+        <DottedBordersWrapper
+          dotColor="bg-white"
+          borderColor="border-white"
+          :borderOpacity="0.6"
+        >
+          <div class="text-white text-center px-4 py-1.5">
+            <p class="text-xl">SON KATILIM TARİHİ</p>
+            <p class="drop-shadow-glow text-3xl font-black">10.04.2022</p>
+          </div>
+        </DottedBordersWrapper>
+
+        <DottedBordersWrapper
+          dotColor="bg-white"
+          borderColor="border-white"
+          :borderOpacity="0.6"
+          class="mb-10 active:scale-95 transition-transform"
+        >
+          <ActionButton> HEMEN BAŞVUR </ActionButton>
+        </DottedBordersWrapper>
       </div>
     </div>
-    <!-- <DottedBordersWrapper
-      dotColor="bg-white"
-      borderColor="border-white"
-      :borderOpacity="0.6"
-      class="mb-10 active:scale-95 transition-transform"
-    >
-      <ActionButton> HEMEN BAŞVUR </ActionButton>
-    </DottedBordersWrapper>
+    <VerticalNavbar />
+    <!--
     <DottedBordersWrapper
       dotColor="white"
       borderColor="white"
@@ -182,8 +76,14 @@
         Some content to display Some content to display Some content to display
         Some content to display
       </div>
-    </DottedBordersWrapper> -->
-    <!-- <div class="bg-green-400 w-32 h-12 absolute z-0">Text</div> -->
+    </DottedBordersWrapper>
+    <div class="bg-green-400 w-32 h-12 absolute z-0">Text</div> -->
+    <section id="about">HAKKIMIZDA</section>
+    <section id="program">PROGRAM</section>
+    <section id="who-can-join">KİMLER KATILABİLİR?</section>
+    <section id="what-will-i-win">NELER KAZANACAĞIM?</section>
+    <section id="application-process">BAŞVURU SÜREÇLERİ</section>
+    <section id="faq">SIKÇA SORULAN SORULAR</section>
   </div>
 </template>
 
@@ -192,3 +92,12 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss" scoped>
+.main-section {
+  background-image: url('~assets/images/BG.png');
+}
+section {
+  color: white;
+}
+</style>
