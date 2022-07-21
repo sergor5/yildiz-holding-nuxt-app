f46c839447dfb173e8cca60337063d780924a299
Sergen ORIN <serg_or5@hotmail.com>
2022-07-21 20:24:36 +0300

    added layout folder, fixed footer

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Nuxt />
+    <footer
+      class="
+        h-40
+        bg-primary
+        w-full
+        flex
+        justify-between
+        items-center
+        text-white
+        px-8
+      "
+    >
+      <div class="flex items-center text-5xl font-bold gap-2">
+        YILDIZ<span><StarIcon /></span> HOLDING
+      </div>
+      <div class="flex items-center">
+        <div class="grid place-items-center p-5">
+          <LinkedinIcon class="fill-current text-white" />
+        </div>
+        <div class="grid place-items-center p-5">
+          <InstagramIcon class="fill-current text-white" />
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -188,30 +188,6 @@
     <section id="what-will-i-win">NELER KAZANACAĞIM?</section>
     <section id="application-process">BAŞVURU SÜREÇLERİ</section>
     <section id="faq">SIKÇA SORULAN SORULAR</section>
-    <footer
-      class="
-        h-40
-        bg-primary
-        w-full
-        flex
-        justify-between
-        items-center
-        text-white
-        px-8
-      "
-    >
-      <div class="flex items-center text-5xl font-bold gap-2">
-        YILDIZ<span><StarIcon /></span> HOLDING
-      </div>
-      <div class="flex items-center">
-        <div class="grid place-items-center p-5">
-          <LinkedinIcon class="fill-current text-white" />
-        </div>
-        <div class="grid place-items-center p-5">
-          <InstagramIcon class="fill-current text-white" />
-        </div>
-      </div>
-    </footer>
   </div>
 </template>
 
