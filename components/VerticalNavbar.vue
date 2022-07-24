c4988962f08e4ae58184950dee93528cab55d11b
Sergen ORIN <serg_or5@hotmail.com>
2022-07-24 21:36:53 +0300

    interactive menu backup

--- components/VerticalNavbar.vue
+++ components/VerticalNavbar.vue
@@ -2,29 +2,39 @@
   <div
     class="
       fixed
-      top-8
-      right-8
+      top-2
+      right-2
+      md:top-8 md:right-8
       flex flex-col
       justify-between
       divide-y divide-white divide-opacity-40
-      h-3/5
-      w-24
-      bg-primary bg-opacity-20
+      md:w-24
+      bg-secondary bg-opacity-70
+      md:bg-primary md:bg-opacity-20
       backdrop-blur-sm
       border border-white border-opacity-40
+      transition-all
     "
-    data-aos="zoom"
-    v-if="visible"
+    :class="{
+      'h-3/5': menuOpen,
+      'h-[9%]': !menuOpen,
+      'w-11/12': menuOpen && isMobile,
+      'w-[91px]': !menuOpen && isMobile,
+    }"
+    data-aos="non-transform-fade"
   >
     <div>
-      <div class="w-full text-center text-white text-base">
-        <div>
-          <MenuIcon class="mx-auto my-2" />
-        </div>
-        <div class="font-semibold">MENU</div>
+      <div class="w-full text-white text-base px-4 pt-2 flex justify-end">
+        <button
+          class="flex flex-col items-end md:items-center"
+          @click="toggleMenuOpen"
+        >
+          <MenuIcon class="my-2" />
+          <div class="font-semibold w-14">MENU</div>
+        </button>
       </div>
-      <nav class="relative w-0 flex justify-end">
-        <ul class="absolute text-white whitespace-nowrap" v-if="menuOpen">
+      <nav class="relative p-5 md:p-0 md:w-0 flex justify-end" v-if="menuOpen">
+        <ul class="absolute text-white whitespace-nowrap">
           <li
             v-for="item in navItems"
             class="nav-item"
@@ -38,12 +48,34 @@
         </ul>
       </nav>
     </div>
-    <div class="w-full">
-      <div class="w-full h-16 py-2 grid place-items-center">
-        <LinkedinIcon class="fill-current text-primary" />
+    <div class="w-full flex md:flex-col" v-if="menuOpen">
+      <div
+        class="w-full h-16 py-2 grid place-items-center"
+        data-aos="fade"
+        data-aos-delay="200"
+      >
+        <LinkedinIcon
+          class="
+            fill-current
+            transition
+            text-white
+            hover:text-primary hover:cursor-pointer
+          "
+        />
       </div>
-      <div class="w-full h-16 py-2 grid place-items-center">
-        <InstagramIcon class="fill-current text-white" />
+      <div
+        class="w-full h-16 py-2 grid place-items-center"
+        data-aos="fade"
+        data-aos-delay="250"
+      >
+        <InstagramIcon
+          class="
+            fill-current
+            transition
+            text-white
+            hover:text-primary hover:cursor-pointer
+          "
+        />
       </div>
     </div>
   </div>
@@ -56,6 +88,7 @@ export default {
       selectedId: 0,
       visible: true,
       menuOpen: true,
+      windowWidth: 768,
       navItems: [
         { id: 0, text: 'HAKKINDA', url: '#about' },
         { id: 1, text: 'PROGRAM', url: '#program' },
@@ -69,12 +102,46 @@ export default {
   methods: {
     selectItem(id) {
       this.selectedId = id
+      if (this.isMobile) {
+        setTimeout(() => {
+          this.menuOpen = false
+        }, 300)
+      }
     },
+    toggleMenuOpen() {
+      this.menuOpen = !this.menuOpen
+    },
+    onResize() {
+      this.windowWidth = window.screen.width
+    },
+  },
+  computed: {
+    isMobile() {
+      return this.windowWidth <= 768
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.windowWidth = window.screen.width
+
+      window.addEventListener('resize', this.onResize)
+    })
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize)
   },
 }
 </script>
 
 <style lang="scss" scoped>
+[data-aos='non-transform-fade'] {
+  opacity: 0;
+
+  &.aos-animate {
+    opacity: 1;
+  }
+}
 .nav-item {
   @apply pr-3 py-2 flex justify-end items-center transition-all hover:pr-4;
 
